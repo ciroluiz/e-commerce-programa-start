e38b487cb07e6ebe6dea1dff0db07a9d897f472c
ciroluiz <ciroluiz.cl@gmail.com>
2025-03-20 17:12:45 -0300

    header component + estilizações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, // Declaração do componente principal
+    // Outros componentes declarados aqui
+    HeaderComponent, // Declaração do componente de cabeçalho
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, // Necessário para aplicações web
+    AppRoutingModule, // Importa o módulo de rotas configurado em app-routing.module.ts
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // Serviços injetáveis (caso aplicável)
+  bootstrap: [AppComponent], // Componente inicial
 })
-export class AppModule { }
+export class AppModule {}
