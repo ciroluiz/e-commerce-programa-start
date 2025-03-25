4402a79b1ed13b327384415b24515770394416e0
ciroluiz <ciroluiz.cl@gmail.com>
2025-03-25 10:18:28 -0300

    feat: cria componente detalhes-produto, template e CSS definidos, lógica pendente e inclui rota /produto/:id para acesso ao componente

---
+++ src/app/produtos/detalhes-produto/detalhes-produto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes-produto',
+  standalone: false,
+  templateUrl: './detalhes-produto.component.html',
+  styleUrl: './detalhes-produto.component.css'
+})
+export class DetalhesProdutoComponent {
+
+}
