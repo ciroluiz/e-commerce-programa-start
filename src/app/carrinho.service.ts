856f73064330e027a3fe9336c0b4a0b38f84ca25
ciroluiz <ciroluiz.cl@gmail.com>
2025-03-31 18:40:39 -0300

    feat: adiciona funcionalidades de remoção e compra de itens do carrinho, atualização do total e exibição dos itens

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -10,12 +10,22 @@ export class CarrinhoService {
   constructor() {}
 
   obtemCarrinho() {
-    return JSON.parse(localStorage.getItem('carrinho') || '');
+    this.itens = JSON.parse(localStorage.getItem('carrinho') || '[]');
+    return this.itens;
   }
 
   adicionarAoCarrinho(produto: IProdutoCarrinho) {
     this.itens.push(produto);
     localStorage.setItem('carrinho', JSON.stringify(this.itens));
+  }
+
+  removerDoCarrinho(produtoId: number) {
+    const index = this.itens.findIndex((item) => item.id === produtoId);
+    if (index !== -1) {
+      this.itens.splice(index, 1); // Remove apenas o primeiro item encontrado
+      localStorage.setItem('carrinho', JSON.stringify(this.itens));
+      location.reload(); // Atualiza a página para refletir a remoção
+    }
   }
 
   limparCarrinho() {
--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarrinhoService } from '../carrinho.service';
+import { IProdutoCarrinho } from '../produtos';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carrinho',
   standalone: false,
   templateUrl: './carrinho.component.html',
-  styleUrl: './carrinho.component.css'
+  styleUrl: './carrinho.component.css',
 })
-export class CarrinhoComponent {
+export class CarrinhoComponent implements OnInit {
+  itensCarrinho: IProdutoCarrinho[] = [];
+  total = 0;
 
+  constructor(
+    public carrinhoService: CarrinhoService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.itensCarrinho = this.carrinhoService.obtemCarrinho();
+    this.calcularTotal();
+  }
+
+  calcularTotal() {
+    this.total = this.itensCarrinho.reduce(
+      (totalCarrinho, item) => totalCarrinho + item.preco * item.quantidade,
+      0
+    );
+  }
+
+  removerDoCarrinho(produtoId: number) {
+    this.itensCarrinho = this.itensCarrinho.filter(
+      (item) => item.id !== produtoId
+    );
+    this.carrinhoService.removerDoCarrinho(produtoId);
+  }
+
+  comprar() {
+    alert('Compra realizada com sucesso!');
+    this.carrinhoService.limparCarrinho();
+    this.router.navigate(['/produtos']);
+  }
 }
